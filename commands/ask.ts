db9c4d59a9b3efbdb4f4e48948cf08f49c3757de
moez-rd <slow9ie@gmail.com>
2024-03-01 17:59:16 +0700

    update

--- commands/ask.ts
+++ commands/ask.ts
@@ -18,7 +18,7 @@ const ask: Command = {
 
     const result = await model.generateContent([
       message.body.substr(message.body.indexOf(" ") + 1),
-      {inlineData: {data: media, mimeType: 'image/png'}}
+      {inlineData: {data: media, mimeType: 'image/jpeg'}}
     ]);
 
     const response = await result.response;
